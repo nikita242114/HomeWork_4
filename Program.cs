b2f07c240668d1e23ea4a91664973e7a8c6b6bd9
nikita242114 <5358711@mail.ru>
2023-04-13 17:21:52 +0500

    continue 1

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+int Prompt(string msg)
+{
+
+System.Console.WriteLine($"{msg}");
+return Convert.ToInt32(Console.ReadLine());
+}
+int number = Prompt("Введите число");
+string ss = "Это не день недели";
+string check(int number)
+{
+if (number >= 6 && number < 8) ss = "Это выходной";
+if (number >= 1 && number < 7) ss = "Это будний день";
+return ss;
+}
+System.Console.WriteLine(check(number));
